950c273d47de95a4b17e6304496faf2a54fbafda
Aidith <47973819+aidith@users.noreply.github.com>
2019-02-25 16:26:22 +0530

    remove comment line

--- getcustomer.php
+++ getcustomer.php
@@ -2,8 +2,7 @@
 require_once('vendor/autoload.php');
 \Stripe\Stripe::setApiKey('sk_test_rct4HMeN9nko8Ir1sjuIb2vf');
 
-// $customer = \Stripe\Customer::retrieve("cus_EXEDcWugq0pk2j");
-
-// echo "<pre>";
-// print_r($customer);
+$customer = \Stripe\Customer::retrieve("cus_EXEDcWugq0pk2j");
+echo "<pre>";
+print_r($customer);
 
